7339b752cc2789f097c630183dda745d221cf02d
Jack Fan <jackfan108@gmail.com>
2017-02-18 18:15:52 -0800

    configure actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set Action Cable server url for consumer connection
+  config.action_cable.url = 'ws://localhost:28080'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   root 'homepage#show'
 
+  mount ActionCable.server => '/cable'
 end
