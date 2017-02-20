c78b8c82d4e17531d89aecb0aa4607dd233db27e
Bogdan Vitoc <bogdanvitoc@gmail.com>
2017-02-20 00:33:18 -0800

    Clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Allow action cable connections from http and https
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
